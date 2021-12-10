b125c3215ce0072efa49d36d95cda2b1b0ea73d1
Harshit Verma <51871652+HarshitVerma1@users.noreply.github.com>
2021-12-10 22:49:53 +0530

    new

--- Coding MInutes Practice/maxSumOfSubArray.cpp
+++ Coding MInutes Practice/maxSumOfSubArray.cpp
@@ -2,14 +2,21 @@
 using namespace std;
 int main()
 {
-    int n, m;
+    int n, len;
     cout << "Enter size of Array1 : ";
     cin >> n;
     int arr1[n];
+    len = sizeof(arr1) / sizeof(arr1[0]);
     for (int i = 0; i < n; i++)
     {
         cin >> arr1[i];
     }
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr1[i] << " ";
+    }
+    cout << endl;
+    sort(arr1, arr1 + len);
     for (int i = 0; i < n; i++)
     {
         cout << arr1[i] << " ";
